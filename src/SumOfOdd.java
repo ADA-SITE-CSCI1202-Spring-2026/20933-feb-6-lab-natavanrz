3a6b90543864709a291f2bee2f7a9283a64d8b29
natavanrz <natavan.rzayeva07@gmail.com>
2026-02-06 14:12:05 +0400

    Natavan Rzayeva lab exercises

---
+++ src/SumOfOdd.java
@@ -0,0 +1,20 @@
+public class SumOfOdd {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 15;
+
+        int start = Math.min(a, b);
+        int end = Math.max(a, b);
+
+        int sum = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (i % 2 != 0) {
+                sum += i;
+            }
+        }
+
+        System.out.println("Sum of odd numbers = " + sum);
+    }
+}
